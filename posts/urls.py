9ef67c03779a29ef1e482e963d04d51f905c87e1
hyuneu-n <hyuneun@skuniv.ac.kr>
2023-08-18 00:25:25 +0900

    hackerton

---
+++ posts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rest_framework import routers
+
+from .views import PostViewSet, CommentViewSet
+
+router = routers.SimpleRouter()
+router.register('posts', PostViewSet)
+router.register('comments', CommentViewSet)
+
+urlpatterns = router.urls
